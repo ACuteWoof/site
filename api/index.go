c1f260bdad7326ab6cae4bcef34416bc8b250f43
Vithushan <sutharshanvithushan@gmail.com>
2024-11-22 05:54:45 +0530

    feat: api

--- api/index.go
+++ api/index.go
@@ -1,10 +1,30 @@
 package handler
- 
+
 import (
-  "fmt"
-  "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
- 
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "Message sent to Woof!")
+
+	var websocket_uri string = os.Getenv("WEBSOCKET_URI")
+
+	postBody, _ := json.Marshal(map[string]string{
+		"content": "@acutewoof, " + r.RemoteAddr + " says: " + r.URL.Query()["message"][0],
+	})
+
+	responseBody := bytes.NewBuffer(postBody)
+
+	resp, err := http.Post(websocket_uri, "application/json", responseBody)
+	fmt.Println(resp)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
