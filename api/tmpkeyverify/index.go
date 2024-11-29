8318473f6deeed6bddace3823e74a2cc552817e3
Vithushan <sutharshanvithushan@gmail.com>
2024-11-29 21:45:35 +0530

    feat: add tmp api

---
+++ api/tmpkeyverify/index.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var adminkey string = os.Getenv("TMP_ADMIN_KEY")
+	license := r.URL.Query()["license"][0]
+	if license == adminkey {
+		fmt.Fprintf(w, "Verified")
+	} else {
+		fmt.Fprintf(w, "No")
+	}
+}
