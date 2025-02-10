e0876c44a7e48e8b0b73c482d52fdbd8a119ccbf
Vithushan <sutharshanvithushan@gmail.com>
2025-02-10 21:46:47 +0530

    fix: update script to include dot files

--- scripts/render.sh
+++ scripts/render.sh
@@ -7,6 +7,7 @@ cd "${0%/*}"
 echo "" > ../public/index.html || touch ../public/index.html
 
 cp -r ../src/public/* ../public/.
+cp -r ../src/public/.* ../public/.
 cat ../src/top2prtop.html >> ../public/index.html
 cat ../extra/projects.html >> ../public/index.html
 cat ../src/prbtm2bltop.html >> ../public/index.html
