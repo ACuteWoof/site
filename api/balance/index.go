babd4f386fb8964e4541dd92a33b4fd55f1aecff
Vithushan <sutharshanvithushan@gmail.com>
2026-01-14 01:56:26 +0530

    api

---
+++ api/balance/index.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"net/http"
+	"io"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// get response from https://70.34.201.17/log.csv and return it as plain text
+	res, _ := http.Get("https://70.34.201.17/log.csv")
+	defer res.Body.Close()
+	bodyBytes, _ := io.ReadAll(res.Body)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write(bodyBytes)
+}
