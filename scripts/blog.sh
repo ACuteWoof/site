4553e5580b50fc66b96d22c4d3b2fd5a07ad9a46
Vithushan <sutharshanvithushan@gmail.com>
2026-01-13 14:14:09 +0530

    pandoc for blog

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -14,6 +14,7 @@ echo "" > ../extra/blog.html || touch ../extra/blog.html
 TEXT=""
 
 while IFS="|" read -r DATE TITLE DESCRIPTION LINK; do
-	TEXT="<div class='blogpost'><h3>$TITLE</h3><div class='!text-xs'>$DATE</div><p>$DESCRIPTION</p><a href='https://rmd.lewoof.xyz?url=https://lewoof.xyz/blog/md/$LINK' target='_blank'>Read</a></div>$TEXT"
+	npx pandoc "../blog/$LINK.md" -s -o "../public/blog/$LINK.html" --template "../src/blog-template.html";
+	TEXT="<div class='blogpost'><h3>$TITLE</h3><div class='!text-xs'>$DATE</div><p>$DESCRIPTION</p><a href='$LINK' target='_blank'>Read</a></div>$TEXT"
 done < ../src/blogposts.txt
 echo $TEXT > ../extra/blog.html
